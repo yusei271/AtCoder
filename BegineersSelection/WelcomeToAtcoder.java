4ed30f04ddb6014a59fd392e17112c121ec6df91
yusei271 <y.fujimoto.24.4.1368@4nonome.com>
2025-12-02 22:05:01 -0800

    Add Welcome

--- BegineersSelection/WelcomeToAtcoder.java
+++ BegineersSelection/WelcomeToAtcoder.java
@@ -1,3 +1,36 @@
+import java.io.*;
+import java.util.*;
+
 public class WelcomeToAtcoder {
-    
+    static FastScanner sc = new FastScanner();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        // a, b, c, s を読む
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        String s = sc.next();
+
+        int sum = a + b + c;
+
+        // a + b + c と s を空白区切りで出力
+        out.println(sum + " " + s);
+        out.flush();
+    }
+
+    // ===== テンプレ =====
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+        String next() {
+            while (!st.hasMoreTokens()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+    }
 }
