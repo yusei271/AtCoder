e186484288a01d8965bac7a157a9b26994a4de68
yusei271 <y.fujimoto.24.4.1368@4nonome.com>
2025-12-03 21:41:06 -0800

    Coins

---
+++ BeginnersSelection/Coins.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Coins {
+    static FastScanner sc = new FastScanner();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int X = sc.nextInt();
+
+        int count = 0;
+
+        // 500: i, 100: j, 50: k
+        for (int i = 0; i <= A; i++) {
+            for (int j = 0; j <= B; j++) {
+                for (int k = 0; k <= C; k++) {
+                    int total = 500 * i + 100 * j + 50 * k;
+
+                    if (total == X) {
+                        count++;
+                    }
+                }
+            }
+        }
+        out.println(count);
+        out.flush();
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+        String next() {
+            while (!st.hasMoreTokens()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+    }
+}
