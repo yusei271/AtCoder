c5d23ff9dd1dd125b17dbe87e8798d5999b4e8e3
yusei271 <y.fujimoto.24.4.1368@4nonome.com>
2025-12-07 13:38:37 -0800

    Kagami Mochi

--- BeginnersSelection/KagamiMochi.java
+++ BeginnersSelection/KagamiMochi.java
@@ -6,8 +6,22 @@ public class KagamiMochi {
     static PrintWriter out = new PrintWriter(System.out);
 
     public static void main(String[] args) {
-        int n = sc.nextInt();
-        out.println(n);
+        int N = sc.nextInt();
+        int[] d = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            d[i] = sc.nextInt();
+        }
+
+        Arrays.sort(d);
+
+        int count = 1;
+        for (int i = 0; i < N - 1; i++) {
+            if (d[i] < d[i + 1]) {
+                count++;
+            }
+        }
+        out.println(count);
         out.flush();
     }
 
