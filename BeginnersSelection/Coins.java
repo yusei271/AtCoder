f4b3e58be481d120589a117dfad7ec70b02caff7
yusei271 <y.fujimoto.24.4.1368@4nonome.com>
2025-12-07 12:20:43 -0800

    Coins

--- BeginnersSelection/Coins.java
+++ BeginnersSelection/Coins.java
@@ -6,8 +6,25 @@ public class Coins {
     static PrintWriter out = new PrintWriter(System.out);
 
     public static void main(String[] args) {
-        int n = sc.nextInt();
-        out.println(n);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int X = sc.nextInt();
+
+        int count = 0;
+
+        for (int i = 0; i <= A; i++) {
+            for (int j = 0; j <= B; j++) {
+                for (int k = 0; k <= C; k++) {
+                    int total = 500 * i + 100 * j + 50 * k;
+
+                    if (total == X) {
+                        count++;
+                    }
+                }
+            }
+        }
+        out.println(count);
         out.flush();
     }
 
