04a1bf21983b9241fb1b52669714fb6777ba8154
yusei271 <y.fujimoto.24.4.1368@4nonome.com>
2025-12-07 11:22:17 -0800

    ShiftOnly

--- BeginnersSelection/ShiftOnly.java
+++ BeginnersSelection/ShiftOnly.java
@@ -6,8 +6,29 @@ public class ShiftOnly {
     static PrintWriter out = new PrintWriter(System.out);
 
     public static void main(String[] args) {
-        int n = sc.nextInt();
-        out.println(n);
+        int N = sc.nextInt();
+        int firstA = sc.nextInt();
+        int minCount = 0;
+
+        while (firstA > 0 && firstA % 2 == 0) {
+            firstA /= 2;
+            minCount++;
+        }
+
+        for (int i = 1; i < N; i++) {
+            int a = sc.nextInt();
+            int count = 0;
+
+            while (a > 0 && a % 2 == 0) {
+                a /= 2;
+                count++;
+            }
+
+            if (count < minCount) {
+                minCount = count;
+            }
+        }
+        out.println(minCount);
         out.flush();
     }
 
