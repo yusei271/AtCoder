ec4dc0ddf50d498a2ef131d18dddf29332a267c5
yusei271 <y.fujimoto.24.4.1368@4nonome.com>
2025-12-06 21:36:59 -0800

    KagamiMochi

---
+++ BeginnersSelection/KagamiMochi.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class KagamiMochi {
+    static FastScanner sc = new FastScanner();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int N = sc.nextInt();
+        int[] d = new int[N];
+        for (int i = 0; i < N; i++) {
+            d[i] = sc.nextInt();
+        }
+
+        Arrays.sort(d);
+
+        int count = 1;
+
+        for (int i = 1; i < N; i++) {
+            if (d[i] != d[i - 1]) {
+                count++;
+            }
+        }
+        out.println(count);
+        out.flush();
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+        String next() {
+            while (!st.hasMoreTokens()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+    }
+}
